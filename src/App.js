c698e4382bcd54e1ea6442c12250fd60bc165792
BADR RAZKI <razkibadr3@gmail.com>
2023-01-28 11:58:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Mov from './Mov';
 
 const nav = ['All','movies', 'anime','series','cartoon']
 const objs = [
-  {name:'Dragon Ball Super Hero', url:'./img/DBS.jpg',cate:'anime'},
-  {name:'Game Of Thrones', url:'./img/GOT.jpg',cate:'series'},
-  {name:'Prisoners', url:'./img/PRI.jpg',cate:'movies'},
-  {name:'The Way Back', url:'./img/TWB.jpg',cate:'movies'},
-  {name:'Hunter X Hunter Phantom Rouge', url:'./img/HXH.jpg',cate:'anime'},
-  {name:'Over The Garden Wall', url:'../img/OTGW.jpg',cate:'cartoon'},
-  {name:'Adventure Time', url:'../img/AT.jpg',cate:'cartoon'},
-  {name:'Breaking Bad', url:'../img/BB.jpg',cate:'series'},
+  {name:'Dragon Ball Super Hero', url:'img/DBS.jpg',cate:'anime'},
+  {name:'Game Of Thrones', url:'img/GOT.jpg',cate:'series'},
+  {name:'Prisoners', url:'img/PRI.jpg',cate:'movies'},
+  {name:'The Way Back', url:'img/TWB.jpg',cate:'movies'},
+  {name:'Hunter X Hunter Phantom Rouge', url:'img/HXH.jpg',cate:'anime'},
+  {name:'Over The Garden Wall', url:'img/OTGW.jpg',cate:'cartoon'},
+  {name:'Adventure Time', url:'img/AT.jpg',cate:'cartoon'},
+  {name:'Breaking Bad', url:'img/BB.jpg',cate:'series'},
 ]
 
 const App = _ => {
